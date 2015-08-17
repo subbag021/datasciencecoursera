ae835172f8137e70fd8925154237dca641e9b905
subbag021 <gottipati02@gmail.com>
2015-08-16 22:40:38 -0700

    changes
    changes

--- Run_Analysis1.R
+++ Run_Analysis1.R
@@ -72,3 +72,22 @@ write.table(result, "data_with_means.txt") # write out the 2nd dataset
 
 
 
+library(plyr)
+library(ggplot2)
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+aggregate.data <- with(NEI, aggregate(Emissions, by = list(year), sum))
+
+
+plot(aggregate.data, type = "o", ylab = expression("Total Emissions, PM"[2.5]), 
+     xlab = "Year", main = "Total Emissions in the United States")
+polygon(aggregate.data, col = "green", border = "blue")
+
+
+
+install.packages("swirl")
+library(swirl)
+install_from_swirl("Statistical Inference")
+swirl()
